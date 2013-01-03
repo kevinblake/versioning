7fbf3c2490be94aabd3503a1e480c5968c58cc75
Kevin Blake <kblake@puzzlebox.co.uk>
2013-01-03 12:42:06 +0000

    Explicitly ignore /version route in sample app

--- dotnet/Puzzlebox.Versioning/App_Start/RouteConfig.cs
+++ dotnet/Puzzlebox.Versioning/App_Start/RouteConfig.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace Puzzlebox.Versioning
@@ -14,12 +10,11 @@ namespace Puzzlebox.Versioning
 			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 			routes.IgnoreRoute("version");
 
-
 			routes.MapRoute(
 				name: "Default",
 				url: "{controller}/{action}/{id}",
-				defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-			);
+				defaults: new {controller = "Home", action = "Index", id = UrlParameter.Optional}
+				);
 		}
 	}
 }
