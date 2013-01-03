652529ca97072a678ca81371b16bc47d76aaa5c9
Kevin Blake <kblake@puzzlebox.co.uk>
2013-01-03 12:42:48 +0000

    Code cleanup on home controller

--- dotnet/Puzzlebox.Versioning/Controllers/HomeController.cs
+++ dotnet/Puzzlebox.Versioning/Controllers/HomeController.cs
@@ -4,14 +4,13 @@ using Puzzlebox.Versioning.Business.Extensions;
 
 namespace Puzzlebox.Versioning.Controllers
 {
-    public class HomeController : Controller
-    {
-        //
-        // GET: /Home/
-        public ActionResult Index()
-        {
+	public class HomeController : Controller
+	{
+		//
+		// GET: /Home/
+		public ActionResult Index()
+		{
 			return Content(VersionInformation.GetVersionInformation().ToJson(), "application/json");
-        }
-
-    }
+		}
+	}
 }
